ca42b0bb1dab297dd45ff254fff68ae0c2a3a981
Linards Naglis <125291166+Linards657@users.noreply.github.com>
2023-03-10 01:09:59 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,36 +1,80 @@
 # python3
+import math
 
 
-def build_heap(data):
+def build_heap(data, n):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    data_save = []
+    steps = 0
+    levels = math.floor(math.log2(n + 1))
+    i = 2 ** (levels + 1) - 1
+    # TODO: Create heap and heap sort
 
+    while i >= -20:
+        if i < 2:
+            if steps < 1:
+                break
+            else:
+                i = steps - 1
+                steps = 0
+        try:
+            if data[i - 1] < data[i // 2 - 1]:
+                swaps.append((i // 2 - 1, i-1))
+                data_save.append((data[i // 2 - 1], data[i-1]))
+                data[i // 2 - 1] = data_save[-1][1]
+                data[i-1] = data_save[-1][0]
+                steps = steps + i
+                i = i // 2
+                print("!!!")
+                continue
+        except IndexError:
+            pass
+        try:
+            if data[i - 2] < data[i // 2 - 1]:
+                swaps.append((i // 2 - 1, i - 2))
+                data_save.append((data[i // 2 - 1], data[i - 2]))
+                data[i // 2 - 1] = data_save[-1][1]
+                data[i - 2] = data_save[-1][0]
+                steps = steps + i
+                i = i // 2
+                print("???")
+                continue
+        except IndexError:
+            pass
+        i = i - 2
+    # try to achieve  O(n) and not O(n2)
 
     return swaps
 
 
 def main():
-    
     # TODO : add input and corresponding checks
-    # add another input for I or F 
+    # add another input for I or F
     # first two tests are from keyboard, third test is from a file
 
-
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    text = input()
+    
+    if "I" in text[:1]:
+        n = int(input())
+        data = list(map(int, input().split()))
+    else:
+        text = input()
+        f = open("./test/" + text, "r")
+        n = f.readline()
+        data = list(map(int, f.readline().split()))
+        f.close()
 
-    # checks if lenght of data is the same as the said lenght
+    # checks if length of data is the same as the said length
     assert len(data) == n
 
-    # calls function to assess the data 
+    # calls function to assess the data
     # and give back all swaps
-    swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
+    swaps = build_heap(data, n)
+
+    # TODO: output how many swaps were made,
     # this number should be less than 4n (less than 4*len(data))
-
 
     # output all swaps
     print(len(swaps))
@@ -40,3 +84,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
